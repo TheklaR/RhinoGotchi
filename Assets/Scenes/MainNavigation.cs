cbc80bd60856a40adfab8cfc760aa17ad631c30b
TheklaR <thekla.roland@gmail.com>
2019-11-09 16:29:36 -0800

    Add main navigation

---
+++ Assets/Scenes/MainNavigation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainNavigation : MonoBehaviour
+{
+	public void GoToScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
