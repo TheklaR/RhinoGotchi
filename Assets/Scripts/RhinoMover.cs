2b5f9caf63add63ea9b09be168f052cc1ce589be
TheklaR <thekla.roland@gmail.com>
2019-11-09 19:38:44 -0800

    Update UI for some scenes and edit script for RhinoMover

--- Assets/Scripts/RhinoMover.cs
+++ Assets/Scripts/RhinoMover.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class RhinoMover : MonoBehaviour
 {
-	public GameObject rhino;
-	public Vector3 targetPosition;
-	public Quaternion targetRotation;
-	private bool isMoving = false;
-	private float progress = Time.deltaTime;
-    // Start is called before the first frame update
-    void Start()
-    {
-		rhino.gameObject.GetComponent<Transform>().position = new Vector3(-10, 0, 0);
-    }
+	public Vector3 stopPosition;
+	private Coroutine _coroutine;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (isMoving)
+	public float moveDuration = 4.0f;
+	void Start()
+	{
+		_coroutine = StartCoroutine(MoveCoroutine(rhino));
+	}
+
+	IEnumerator MoveCoroutine(GameObject gObject)
+	{
+		float t = 0.0f;
+		Vector3 start = gObject.GetComponent<Transform>().position;
+
+		while (t < moveDuration)
 		{
-			var current = rhino.GetComponent<Transform>().position;
-			var newPos = Vector3.Lerp(current, targetPosition, progress*Time.deltaTime);
+			t += Time.deltaTime;
+			transform.position = Vector3.Lerp(start, stopPosition, t / moveDuration);
+			yield return null;
 		}
-    }
+	}
 }
